7230c64b44fa9ca1fc5dd662587f8a1a5ad85ed7
Miles <krubbles@gmail.com>
2026-01-11 19:01:31 -0800

    Added test for game database

--- UnitTests/SerializationTests.cs
+++ UnitTests/SerializationTests.cs
@@ -1,6 +1,6 @@
-﻿using Ramen.Game;
+﻿namespace Ramen.UnitTests;
 
-namespace Ramen.UnitTests;
+using Ramen.Game;
 
 public class SerializationTests
 {
@@ -24,7 +24,7 @@ public class SerializationTests
     [Test]
     public void SerializationWorks()
     {
-        FastRandom random = new(10);
+        FastRandom random = new(8);
         for (int i = 0; i < 5; ++i)
         {
             GameState gameState = new(GameData.Default);
@@ -38,5 +38,23 @@ public class SerializationTests
             deserializedGameState.Deserialize(read);
             Assert.That(deserializedGameState.GetHashCode(), Is.EqualTo(gameState.GetHashCode()), "GameState hash code before and after serialization don't match.");
         }
+    }
+
+    [Test]
+    public void GameDatabaseWorks()
+    {
+        GameDatabase gdb = new("Test", false, true);
+        FastRandom random = new(7);
+        for (int i = 0; i < 10; ++i)
+        {
+            GameState gameState = new(GameData.Default);
+            gameState.PlayRandomGame(random);
+
+            gdb.AddGame(gameState);
+        }
+        List<GameState> games = new();
+        foreach (GameState game in gdb)
+            games.Add(game);
+        Assert.That(games, Has.Count.EqualTo(10), "Loading from database didn't return same number of games that were saved.");
     }
 }
