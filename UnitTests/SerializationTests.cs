c20a28ec5f0b28365287699587d61b03488a66c1
Miles <krubbles@gmail.com>
2026-01-11 17:30:47 -0800

    Serialization unit tests

--- UnitTests/SerializationTests.cs
+++ UnitTests/SerializationTests.cs
@@ -1,5 +1,42 @@
-﻿namespace Ramen.UnitTests;
+﻿using Ramen.Game;
+
+namespace Ramen.UnitTests;
 
 public class SerializationTests
 {
+    [Test]
+    public void SerializeDoesSomething()
+    {
+        FastRandom random = new(10);
+        for (int i = 0; i < 5; ++i)
+        {
+            GameState gameState = new(GameData.Default);
+            gameState.PlayRandomGame(random);
+            using MemoryStream stream = new();
+            Assert.That(() =>
+            {
+                gameState.Serialize(stream);
+            }, Throws.Nothing, "Serialization threw an exception.");
+            Assert.That(stream.Length, Is.GreaterThan(0), "Serialization produced an empty byte buffer.");
+        }
+    }
+
+    [Test]
+    public void SerializationWorks()
+    {
+        FastRandom random = new(10);
+        for (int i = 0; i < 5; ++i)
+        {
+            GameState gameState = new(GameData.Default);
+            gameState.PlayRandomGame(random);
+
+            using MemoryStream stream = new();
+            gameState.Serialize(stream);
+
+            using MemoryStream read = new(stream.ToArray(), false);
+            GameState deserializedGameState = new(GameData.Default);
+            deserializedGameState.Deserialize(read);
+            Assert.That(deserializedGameState.GetHashCode(), Is.EqualTo(gameState.GetHashCode()), "GameState hash code before and after serialization don't match.");
+        }
+    }
 }
---
+++ UnitTests/UnitTestUtils.cs
@@ -0,0 +1,19 @@
+﻿namespace Ramen.UnitTests;
+
+using Ramen.Game;
+
+public static class GameStateUnitTestExtentions
+{
+    public static void PlayRandomGame(this GameState gameState, FastRandom random)
+    {
+        while (true)
+        {
+            gameState.AdvanceToNextPlayerChoice();
+            List<Move> moves = gameState.GetMoveOptions();
+            if (moves.Count == 0)
+                return;
+            int moveIndex = random.Next(moves.Count);
+            moves[moveIndex].Apply(gameState);
+        }
+    }
+}
